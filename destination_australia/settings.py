996f6a9b9e172db695359f8dc8d0eef0da17568b
Al-Ouest <alouest_44@yahoo.fr>
2015-06-16 12:49:34 +0200

    changes on way to serve static files

--- destination_australia/settings.py
+++ destination_australia/settings.py
@@ -106,7 +106,7 @@ STATIC_ROOT = (
     os.path.join(BASE_DIR, 'static')
 )
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "travel/static"),
+    # os.path.join(BASE_DIR, "travel/static"),
 )
 # Media files
 
