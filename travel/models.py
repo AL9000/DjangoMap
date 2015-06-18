ee3230ab6a37448e4f21b9d55180772b8e53227c
Al-Ouest <alouest_44@yahoo.fr>
2015-06-18 15:52:24 +0200

    Changing geocoder for googlev3

--- travel/models.py
+++ travel/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.utils.text import slugify
 from django_markdown.models import MarkdownField
 import itertools
-from geopy.geocoders import Nominatim
+from geopy.geocoders import GoogleV3
 
 
 class Milestone(models.Model):
     name = models.CharField(max_length=100)
-    latitude = models.FloatField(editable=False)
-    longitude = models.FloatField(editable=False)
+    latitude = models.FloatField(blank=True,
+                                 help_text="Latitude and longitude fields will be "
+                                           "filled up automatically from city name above")
+    longitude = models.FloatField(blank=True)
     arrival_date = models.DateTimeField()
     text = MarkdownField()
 
@@ -20,7 +22,7 @@ class Milestone(models.Model):
             if not Milestone.objects.filter(slug=self.slug).exists():
                 break
             self.slug = '%s-%d' % (orig, x)
-        geolocator = Nominatim()
+        geolocator = GoogleV3()
         location = geolocator.geocode(self.name)
         self.latitude = location.latitude
         self.longitude = location.longitude
