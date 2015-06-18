a6d7bb75770b0232185ad3d2215aa2f187822edf
Al-Ouest <alouest_44@yahoo.fr>
2015-06-18 02:29:04 +0200

    rmoving some medias files

--- travel/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from travel.models import Comment
-
-
-class CommentForm(ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content', 'user']
